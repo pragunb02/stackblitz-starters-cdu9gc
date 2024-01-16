86be45b537b65fa5fca953fb9b80f862cc1d9c3a
pragunb02 <pragun.bhutani897@gmail.com>
2024-01-16 20:18:08 +0530

    idk

--- models/ecommerce/order.model.js
+++ models/ecommerce/order.model.js
@@ -1,3 +1,43 @@
 import mongoose from "mongoose"
 
-const orderSchema = new mongoose.Schema
+const orderItemSchema = new mongoose.Schema({
+    productId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Product',
+    },
+    quantity:{
+        type:Number,
+        required:true,
+    }
+});
+
+const orderSchema = new mongoose.Schema({
+    orderPrice:{
+
+    },
+    customer:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+    },
+    orderItems:{
+        type: [orderItemSchema],
+        // mutliple items  thats why array of all orders
+
+        //alternate
+        // type:[
+        //     {
+        //         productId:{
+        //             type:mongoose.Schema.Types.ObjectId,
+        //             ref:'Product',
+        //         },
+        //         quantity:{
+        //             type:Number,
+        //             required:true,
+        //         }
+        //     }
+        // ]
+    }
+},{timestamps:true});
+
+
+export const Order=mongoose.model('Order',orderSchema);
